2987d1386b99afc280f7ebaab1183a61e10978e7
Yagya Singh Rajawat <yagya@iboxz.in>
2020-08-06 13:44:59 +0530

    header

--- src/components/navbarPage/NavbarPage.js
+++ src/components/navbarPage/NavbarPage.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse } from "mdbreact";
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBContainer,
+    MDBView, MDBMask } from "mdbreact";
 import { BrowserRouter as Router } from 'react-router-dom';
 import './NavbarPage.css';
 import logo_image from '../../assets/img/logo_white.png';
+import back_head from '../../assets/img/stock/back-1.jpg';
 
 
 class NavbarPage extends Component {
@@ -19,57 +21,67 @@ class NavbarPage extends Component {
 
     render() {
         return (
-            <Router>
-                <MDBNavbar color="stylish-color" dark expand="md">
-                    <MDBNavbarBrand>
-                        <img src={logo_image} style={{width: 150}} alt='Om Agro Food Products' className='image-fluid' />
-                    </MDBNavbarBrand>
-                    <MDBNavbarToggler onClick={this.toggleCollapse} />
-                    <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-                        <MDBNavbarNav right>
-                            <MDBNavItem active>
-                                <MDBNavLink to="#!">Home</MDBNavLink>
-                            </MDBNavItem>
-                            <MDBNavItem>
-                                <MDBNavLink to="#!">Products</MDBNavLink>
-                            </MDBNavItem>
-                            <MDBNavItem>
-                                <MDBNavLink to="#!">Recipes</MDBNavLink>
-                            </MDBNavItem>
-                            <MDBNavItem>
-                                <MDBNavLink to="#!">Gallery</MDBNavLink>
-                            </MDBNavItem>
-                            <MDBNavItem>
-                                <MDBNavLink to="#about_us"> About Us</MDBNavLink>
-                                {/*<MDBDropdown>*/}
-                                {/*    <MDBDropdownToggle nav caret>*/}
-                                {/*        <div className="d-none d-md-inline">Dropdown</div>*/}
-                                {/*    </MDBDropdownToggle>*/}
-                                {/*    <MDBDropdownMenu className="dropdown-default">*/}
-                                {/*        <MDBDropdownItem href="#!">Action</MDBDropdownItem>*/}
-                                {/*        <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>*/}
-                                {/*        <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>*/}
-                                {/*        <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>*/}
-                                {/*    </MDBDropdownMenu>*/}
-                                {/*</MDBDropdown>*/}
-                            </MDBNavItem>
-                        </MDBNavbarNav>
-                        {/*<MDBNavbarNav right>*/}
-                        {/*    <MDBNavItem>*/}
-                        {/*        <MDBNavLink className="waves-effect waves-light" to="#!">*/}
-                        {/*            <a style={{*/}
-                        {/*                textDecoration: "none",*/}
-                        {/*                color: "white"*/}
-                        {/*            }}*/}
-                        {/*               href="mailto:info@omagrofoodproducts.com">*/}
-                        {/*                <MDBIcon fas icon="envelope" />*/}
-                        {/*            </a>*/}
-                        {/*        </MDBNavLink>*/}
-                        {/*    </MDBNavItem>*/}
-                        {/*</MDBNavbarNav>*/}
-                    </MDBCollapse>
-                </MDBNavbar>
-            </Router>
+            <div>
+                <header>
+                    <Router>
+                        <MDBNavbar color="default-color-dark" dark expand="md" fixed="top">
+                            <MDBNavbarBrand href="/">
+                                <img src={logo_image} alt='Om Agro Food Products'
+                                     className='image-fluid logo' />
+                            </MDBNavbarBrand>
+                            {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
+                            <MDBCollapse isOpen={this.state.collapse} navbar>
+                                <MDBNavbarNav right>
+                                    <MDBNavItem active>
+                                        <MDBNavLink to="#!">Home</MDBNavLink>
+                                    </MDBNavItem>
+                                    <MDBNavItem>
+                                        <MDBNavLink to="#!">Products</MDBNavLink>
+                                    </MDBNavItem>
+                                    <MDBNavItem>
+                                        <MDBNavLink to="#!">Recipes</MDBNavLink>
+                                    </MDBNavItem>
+                                    <MDBNavItem>
+                                        <MDBNavLink to="#!">Gallery</MDBNavLink>
+                                    </MDBNavItem>
+                                    <MDBNavItem>
+                                        <MDBNavLink to="#about_us"> About Us</MDBNavLink>
+                                        {/*<MDBDropdown>*/}
+                                        {/*    <MDBDropdownToggle nav caret>*/}
+                                        {/*        <div className="d-none d-md-inline">Dropdown</div>*/}
+                                        {/*    </MDBDropdownToggle>*/}
+                                        {/*    <MDBDropdownMenu className="dropdown-default">*/}
+                                        {/*        <MDBDropdownItem href="#!">Action</MDBDropdownItem>*/}
+                                        {/*        <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>*/}
+                                        {/*        <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>*/}
+                                        {/*        <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>*/}
+                                        {/*    </MDBDropdownMenu>*/}
+                                        {/*</MDBDropdown>*/}
+                                    </MDBNavItem>
+                                </MDBNavbarNav>
+                            </MDBCollapse>
+                        </MDBNavbar>
+                    </Router>
+
+                    <MDBView src={back_head}>
+                        <MDBMask overlay="black-strong" className="flex-center flex-column text-white text-center">
+                            <h1 className="display-1">Amrut</h1>
+                            <h5>The hot gossip is our secret masala and granny tales-the best known preservative</h5>
+                            <br />
+                            <p>Full page intro with background image will be always displayed in full screen mode, regardless of device </p>
+                        </MDBMask>
+                    </MDBView>
+                </header>
+
+                <main>
+                    <MDBContainer className="text-center my-5">
+                        <p align="justify">
+                            Our Vision is expand globally and be recognized as an Indian Household name around the world.
+                            We are committed to grow with integrity.
+                        </p>
+                    </MDBContainer>
+                </main>
+            </div>
         );
     }
 }
